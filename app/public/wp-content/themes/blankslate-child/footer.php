d35585c7360fc4a4358e43d52649e918096fcb9e
Merouabella <131451877+Merouabella@users.noreply.github.com>
2023-10-02 15:42:58 +0100

    initialisation

---
+++ app/public/wp-content/themes/blankslate-child/footer.php
@@ -0,0 +1,15 @@
+
+</main>
+</div>
+<footer id="footer" role=“contentinfo”>
+<?php wp_nav_menu([
+  'theme_location' => 'footer',
+  'container' => false,
+  'menu_class' => 'navbar-nav mr-auto',
+            ]) ;
+            ?>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ app/public/wp-content/themes/blankslate-child/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+<head>
+<meta charset=“<?php bloginfo( 'charset' ); ?>” />
+<meta name=“viewport” content=“width=device-width” />
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="wrapper" class="hfeed">
+<header id="header" role="banner">
+<?php if(has_custom_logo()) : ?>
+<a class="logo-brand" href=“<?php bloginfo('url'); ?>“><?php the_custom_logo(); ?></a>
+ <?php else : ?>
+<a class="site-brand" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+<?php endif; ?>
+<nav id=“menu” role=“navigation” itemscope itemtype=“https://schema.org/SiteNavigationElement”>
+<?php wp_nav_menu([
+            'theme_location' => 'header',
+            'container' => false,
+            'menu_class' => 'navbar-nav mr-auto',
+]);
+            ?>
+</nav>
+</header>
+<div id="container">
+<main id="content" role="main">
